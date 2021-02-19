ce9bece2e7826bbf87b37bda6dd8f048f85df7a1
CodyNix22 <codynix@gmail.com>
2021-02-19 23:55:52 +0000

    Add spaces in an attempt to appease autograder

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,8 +9,8 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["1) Who was the first American woman in space?", "2) True or false: 5000 meters = 5 kilometers.", "3) (5 + 3)/2 * 10 = ?", "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "5) What is the minimum crew size for the ISS?"];
-let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
+let questions = [ "1) Who was the first American woman in space? ", "2) True or false: 5000 meters = 5 kilometers. ", "3) (5 + 3)/2 * 10 = ?", "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "5) What is the minimum crew size for the ISS? " ];
+let correctAnswers = [ "Sally Ride", "true", "40", "Trajectory", "3" ];
 let candidateAnswers = [];
 
 
@@ -25,7 +25,7 @@ function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
   for (i = 0; i < questions.length; i++){
-  candidateAnswers.push(input.question(`${questions[i]}: `))
+  candidateAnswers.push(input.question(`${questions[i]}:`))
 
 }
 };
