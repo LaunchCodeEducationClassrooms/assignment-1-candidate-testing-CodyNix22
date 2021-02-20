3082132cfcff1f2b260ee51ae57ff88cb1577286
CodyNix22 <codynix@gmail.com>
2021-02-20 00:01:46 +0000

    add question #'s iterate with i instead of text embedded in questions

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,7 +9,7 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = [ "1) Who was the first American woman in space? ", "2) True or false: 5000 meters = 5 kilometers. ", "3) (5 + 3)/2 * 10 = ?", "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "5) What is the minimum crew size for the ISS? " ];
+let questions = [ " Who was the first American woman in space? ", " True or false: 5000 meters = 5 kilometers. ", " (5 + 3)/2 * 10 = ?", " Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", " What is the minimum crew size for the ISS? " ];
 let correctAnswers = [ "Sally Ride", "true", "40", "Trajectory", "3" ];
 let candidateAnswers = [];
 
@@ -35,7 +35,7 @@ function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
     console.log(`\nCandidate Name: ${candidateName}`)
   for (i = 0; i < questions.length; i++){
-    console.log(`${questions[i]}\nYour Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}\n`);
+    console.log(`${[i+1]})${questions[i]}\nYour Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}\n`);
   }
 
 
